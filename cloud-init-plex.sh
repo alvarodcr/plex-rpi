6afb23044a24e9ce0d4c1d8f50183e08c7fa2792
alvarodcr <122696431+alvarodcr@users.noreply.github.com>
2023-02-12 19:53:49 +0100

    Add files via upload

---
+++ cloud-init-plex.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+amazon-linux-extras install -y docker
+service docker start
+usermod -a -G docker ec2-user
+pip3 install docker-compose
+mkdir -p ~/plex
+cd ~/plex/
+wget https://raw.githubusercontent.com/alvarodcr/plex-rpi/master/.env
+wget https://raw.githubusercontent.com/alvarodcr/plex-rpi/master/docker-compose.yaml
+docker-compose up -d
+docker stop plex_transmission_1
+cd /transmission/
+jq '.rpc-whitelist-enabled |= false' config.json > config.json.tmp && mv config.json.tmp config.json
+docker start plex_transmission_1
