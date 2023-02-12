ab55d743bf2acba4cf3c9a154bfa69b4e209ddc9
alvarodcr <122696431+alvarodcr@users.noreply.github.com>
2023-02-12 20:56:35 +0100

    Update cloud-init-plex.sh

--- cloud-init-plex.sh
+++ cloud-init-plex.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 sudo yum update -y && sudo yum install -y \
-     curl \
      gnupg2 \
-     vim \
      fail2ban \
-     ntfs-3g
 amazon-linux-extras install -y docker
 service docker start
 usermod -a -G docker ec2-user
 pip3 install docker-compose
-mkdir -p ~/plex
+sudo mkdir -p ~/plex
 cd ~/plex/
 wget https://raw.githubusercontent.com/alvarodcr/plex-rpi/master/.env
 wget https://raw.githubusercontent.com/alvarodcr/plex-rpi/master/docker-compose.yaml
